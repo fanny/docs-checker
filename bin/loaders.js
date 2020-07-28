9228be088479cc079f5aa760cd2a8209fd1e512c
fanny <fanny.vieira@ccc.ufcg.edu.br>
2020-07-28 08:34:03 -0300

    feat(v1): Add cascading algorithm

--- bin/loaders.js
+++ bin/loaders.js
@@ -40,22 +40,31 @@ function loadRules() {
 }
 
 
-function loadConfigFile(projectDir, files) {
-  const localConfigPath = path.join(path.join(projectDir, path.dirname(files[0])), CONFIG_FILENAME);
-  const rootConfigPath = path.join(projectDir, CONFIG_FILENAME);
+function loadConfigFileInAncestors(directoryPath, rootPath){
+  const configPath = path.join(directoryPath, CONFIG_FILENAME);
+  const parentPath = path.dirname(directoryPath);
 
-  if(fs.existsSync(localConfigPath)){
-    return path.resolve(localConfigPath);
-  } else if(fs.existsSync(rootConfigPath)){
-    return path.resolve(rootConfigPath);
+  if(fs.existsSync(configPath)){
+    return path.resolve(configPath);
   } else {
-    throw `Cannot read config file: ${CONFIG_FILENAME}\n`
+    if(parentPath === rootPath){
+      throw `Cannot read config file: ${CONFIG_FILENAME}\n`
+    } else {
+      return loadConfigFileInAncestors(parentPath);
+    }
   }
+}
+
+
+function loadConfigFile(projectDir, files) {
+  const localConfigPath = path.join(projectDir, path.dirname(files[0]));
+  const rootConfigPath = path.join(projectDir, CONFIG_FILENAME);
+  return loadConfigFileInAncestors(localConfigPath, rootConfigPath);
 }
 
 // TODO: Add validations
 function loadOptions(files, projectDir = process.cwd()) {
-  const configFile = loadConfigFile(projectDir, files)
+  const configFile = loadConfigFile(projectDir, files);
   const { rules: userRulesConfig } = require(configFile);
   //const userRulesDir = require(path.resolve(projectDir, rulesDir));
 
